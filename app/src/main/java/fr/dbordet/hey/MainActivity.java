959293ad45011ceddc12d873f6095240b358955e
dennisman <dennis.bordet@soprasteria.com>
2017-11-14 13:35:21 +0100

    Ajout de l'identifiant publicitaire et device test sur mon OP3T

--- app/src/main/java/fr/dbordet/hey/MainActivity.java
+++ app/src/main/java/fr/dbordet/hey/MainActivity.java
@@ -4,16 +4,19 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Remove the below line after defining your own ad unit ID.
-    private static final String TOAST_TEXT = "Test ads are being shown. "
-            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+    /**
+     * Identifiant de ton téléphone.
+     * Utile pour empecher d'avoir des pubs et fausser les résultats publicitaires.
+     * <p>
+     * "Testing with real ads (even if you never tap on them) is against AdMob policy and can cause your account to be suspended".
+     * https://developers.google.com/admob/android/banner?hl=fr
+     */
+    private static final String MY_OP3T_TESTDEVICE_ID = "44572ECFAADF7A1CC5E67FB1FB0CA747";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // Load an ad into the AdMob banner view.
         AdView adView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
+                .setRequestAgent("android_studio:ad_template").addTestDevice(MY_OP3T_TESTDEVICE_ID).build();
         adView.loadAd(adRequest);
         final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.hey);
         this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
@@ -34,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.start();
             }
         });
-        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
     }
 
 }
