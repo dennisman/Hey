e4174a45a1debbbc7551f14d3d82ae526ec2b681
dennisman <dennisman@laposte.net>
2018-01-09 13:53:59 +0100

    Ajout d'une page de paramètres avec des faux paramètres pour l'instant

--- app/src/main/java/fr/dbordet/hey/activity/MainActivity.java
+++ app/src/main/java/fr/dbordet/hey/activity/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -88,6 +91,12 @@ public class MainActivity extends AppCompatActivity implements DialogSoundManage
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
     /**
      * Gere l'action a effectué lorsque le son est désactivé et que l'utilisateur souhaite appuyer sur
      * le bouton hey de l'activité
@@ -119,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements DialogSoundManage
      * initialisation des différents composants
      */
     private void init() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         audioManager = InitHelper.initAudioManager(this.getApplicationContext());
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O || !this.getSystemService(ActivityManager.class).isLowRamDevice()) {
             initAdView();
@@ -211,5 +221,15 @@ public class MainActivity extends AppCompatActivity implements DialogSoundManage
     @Override
     public void back() {
         isDialogLoaded = false;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
